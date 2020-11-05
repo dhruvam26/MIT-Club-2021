3ef8207dbe210b39d25255248e700066a10ce3b1
Dhruva Mahadevan <dhruva.mahadevan@gmail.com>
2020-11-05 19:22:47 +0000

    Initial commit

---
+++ lesson1.py
@@ -0,0 +1,45 @@
+#single line comments are done using hashes
+"""
+Multiple line comments are done
+using triple quotation marks
+"""
+
+#you can print  anything to your terminal using you print function print()
+print('Yay my first print!')
+
+#A variable is assigning any value to some letter or word
+varX = 1
+
+#There are many different types of things you can make a variable
+
+#strings are sentences or words in quotes
+strX = str(varX)
+#you can combine strings using a +
+print('I made the string "' + strX + '"')
+
+#integers are nondecimal numbers
+intX = int(varX)
+#you can use any operator on integers
+print(intX + 4 * 3 - 2 / 1 )
+
+#floats are decimal numbers
+floatX = float(varX)
+#you can do any operator on floats
+print(floatX + 4.3 * 3.2 - 2.1 / 1.0)
+
+#booleans are true or false values
+boolX = bool(varX)
+#booleans default as True
+print(boolX)
+
+#you can change all of the variables as well
+strX = "string"
+intX = 2
+floatX = 3.3
+boolX = False
+#now if we copy all of our print statements
+print('I made the string "' + strX + '"')
+print(intX + 4 * 3 - 2 / 1 )
+print(floatX + 4.3 * 3.2 - 2.1 / 1.0)
+print(boolX)
+
---
+++ main.py
@@ -0,0 +1,13 @@
+#functions are methods you define to do a task
+def funcX():
+  print('Yay I made my first function')
+funcX()
+
+#inputs are data users give while the program runs
+def funcY(inputStr):
+  print(inputStr)
+funcY(input("Give your program a string: "))
+
+def funcZ(inputInt):
+  print(inputInt + 1)
+funcZ(int(input("Give your program an integer: ")))
