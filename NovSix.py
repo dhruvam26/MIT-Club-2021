cb072a674f2aa19008ea856956efd6196cb266bb
Dhruva Mahadevan <dhruva.mahadevan@gmail.com>
2020-11-06 20:06:13 +0000

    First thing

---
+++ NovSix.py
@@ -0,0 +1,17 @@
+
+bool # true and false
+str # "In double quotes its a statement"
+int # 3
+float # 3.23432
+
+eadfsf = True
+asdfae = "string"
+egfes = 3242
+asf = 121.1
+
+helloWorld = "Hello World"
+print(helloWorld)
+
+x= 123 - 2321
+
+print(x)
