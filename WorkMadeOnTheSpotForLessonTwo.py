e57ba0945960b6c95e55e3c660662be2b6c5a59a
Res Sharpe <roberts848@students.abss.k12.nc.us>
2020-11-13 20:18:50 +0000

    Added the work from 11/13/20

---
+++ WorkMadeOnTheSpotForLessonTwo.py
@@ -0,0 +1,29 @@
+
+
+exampleInt = 2232
+exampleFloat = 23.23
+exampleString = "dfsfsdf"
+isThisABoolean = True
+
+
+print(exampleInt)
+exampleInput = float(input())
+
+print()
+print(exampleInput)
+print(type(exampleInput))
+
+print("enter something")
+input()
+
+a = input("Enter something: ")
+print(a)
+
+
+
+def exampleFunction(a, b):
+  c = a + b
+  print(c)
+
+
+exampleFunction(float(input("enter a number ")), float(input("enter a number ")))
